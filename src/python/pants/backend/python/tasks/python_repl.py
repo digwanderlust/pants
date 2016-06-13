5d56bd51731589b2400a612e67f39eb7b8624b73
Matt Olsen <digwanderlust@gmail.com>
2016-06-13 14:11:47 -0700

    fix style issues

--- src/python/pants/backend/python/tasks/python_repl.py
+++ src/python/pants/backend/python/tasks/python_repl.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import os
+
 from pex.pex_info import PexInfo
 
 from pants.backend.python.python_requirement import PythonRequirement
